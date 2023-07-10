81cb601ffc0effb353b71e77e32b30abf75f0b07
sigristarisa <arisasigrist.ch@gmail.com>
2023-07-10 16:19:44 +0200

    created the basic logic of filter

--- src/pages/components/Filter.tsx
+++ src/pages/components/Filter.tsx
@@ -1,28 +1,32 @@
-import { MakeType, Carlistings } from "../../helpers/types";
+import { MakeType, ModelType, Carlisting } from "../../helpers/types";
 
 // Filter component shows selected option based on makeFilter value
 // and can update the makeFilter value on user input
+
+interface FilterType {
+  makeId: MakeType["id"] | undefined;
+  modelId: ModelType["id"] | undefined;
+}
 const Filter = ({
   makes,
-  makeFilter,
-  setMakeFilter,
+  filter,
+  setFilter,
   filteredCarlistings,
 }: {
   makes: MakeType[];
-  makeFilter: MakeType["id"];
-  setMakeFilter: (newMakeFilter: MakeType["id"]) => void;
-  filteredCarlistings: Carlistings[];
+  filter: FilterType;
+  setFilter: (newFilter: FilterType) => void;
+  filteredCarlistings: Carlisting[];
 }) => {
   const handleFilter = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setMakeFilter(Number(e.target.value));
+    const { name, value } = e.target;
+    setFilter({ ...filter, [name]: Number(value) });
   };
-
-  console.log("makeFilter", makeFilter);
 
   return (
     <div>
       <div className="make-filter">
-        <select name="make" onChange={handleFilter} value={makeFilter}>
+        <select name="make" onChange={handleFilter} value={filter.makeId}>
           <option value={undefined}>select Make</option>
           {makes.map((make: MakeType) => (
             <option key={make.id} value={make.id}>
@@ -32,14 +36,11 @@ const Filter = ({
         </select>
       </div>
       <div className="model-filter">
-        <select
-          name="model"
-          onChange={handleFilter}
-          disabled={!makeFilter ? true : false}>
+        <select name="model" onChange={handleFilter}>
           <option value={undefined}>select Model</option>
-          {filteredCarlistings.map((cl: Carlistings) => (
-            <option key={cl.model.id} value={cl.model.id}>
-              {cl.model.name}
+          {filteredCarlistings.map((cl: Carlisting) => (
+            <option key={cl.model?.id} value={cl.model?.id}>
+              {cl.model?.name}
             </option>
           ))}
         </select>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import CarList from "./components/CarList";
 import Filter from "./components/Filter";
 import Car from "../helpers/models/carlisting";
 import Make from "../helpers/models/make";
-import { Carlisting, MakeType } from "../helpers/types";
+import { Carlisting, MakeType, ModelType } from "../helpers/types";
 
 export const getStaticProps: GetStaticProps = async () => {
   const carRes = await Car.findAll();
@@ -29,45 +29,83 @@ const Home = ({
   // No data fetching is necessary.
 
   const defaultFilter = {
-    make: undefined,
-    model: undefined,
-    priceMin: undefined,
-    priceMax: undefined,
-    mileageMin: undefined,
-    mileageMax: undefined,
-    gearbox: undefined,
-    fuel: undefined,
-    drive: undefined,
+    makeId: undefined,
+    modelId: undefined,
+    // priceMin: undefined,
+    // priceMax: undefined,
+    // mileageMin: undefined,
+    // mileageMax: undefined,
+    // gearbox: undefined,
+    // fuel: undefined,
+    // drive: undefined,
   };
+
+  interface FilterType {
+    makeId: MakeType["id"] | undefined;
+    modelId: ModelType["id"] | undefined;
+  }
 
   // Only piece of state is which make id is selected in the make filter.
   // No selected filter value is represented by a falsey value (undefined or empty string).
   // Initial value is undefined. no option is selected in the select element.
-  const [makeFilter, setMakeFilter] = useState<MakeType["id"] | undefined>(
-    undefined
-  );
+  const [filter, setFilter] = useState<FilterType>(defaultFilter);
   // IMPORTANT TO UNDERSTAND: calling the setMakeFilter function provided by useState rerenders this component!
 
   // Filter all car listings to select the ones for the current makeFilter value.
   // If no makeFilter is set, all car listings are shown. ( x ? y : z notation is called "ternary operator")
-  const filteredCarlistings = carlisting.filter((cl: Carlisting) =>
-    makeFilter ? cl.makeId === makeFilter : true
-  );
 
-  console.log("filtered cl", filteredCarlistings);
+  const filterWithValues = (): FilterType | {} => {
+    let newFilter = {};
+    for (const category in filter) {
+      if (filter[category]) {
+        newFilter = { ...newFilter, [category]: filter[category] };
+      }
+    }
+    return newFilter;
+  };
+
+  const filterCarlisting = () => {
+    const filterEntries = Object.entries(filterWithValues());
+    if (!filterEntries.length) return carlisting;
+    let filteredCarlisting;
+    console.log("filtered", filteredCarlisting);
+    for (let i = 0; i < filterEntries.length; i++) {
+      filteredCarlisting = carlisting.filter((cl: Carlisting) => {
+        const key = filterEntries[i][0];
+        const value = filterEntries[i][1];
+        return cl[key].id === value;
+      });
+    }
+    return filteredCarlisting;
+  };
+
+  console.log("filterCarlisting", filterCarlisting());
+
+  // const filteredCarlistings = carlisting.filter((cl: Carlisting) => {
+  //   const filterEntries = Object.entries(filterWithValues());
+  //   let filteredArr;
+  //   for (const filterEntry of filterEntries) {
+  //     const key = filterEntry[0];
+  //     const value = filterEntry[1];
+  //     console.log("cl[key]", cl[key], "value", value);
+  //     return (filteredArr = cl[key].id === value);
+  //   }
+  //   return true;
+  // });
+
   return (
     <Layout>
       <main className="flex justify-center w-9/12 mx-auto border-2 border-red-500 border-solid">
         <aside className="w-3/12">
           <Filter
             makes={makes}
-            makeFilter={makeFilter}
-            setMakeFilter={setMakeFilter}
-            filteredCarlistings={filteredCarlistings}
+            filter={filter}
+            setFilter={setFilter}
+            filteredCarlistings={filterCarlisting()}
           />
         </aside>
         <aside className="w-9/12">
-          <CarList carlisting={filteredCarlistings} />
+          <CarList carlisting={filterCarlisting()} />
         </aside>
       </main>
     </Layout>
